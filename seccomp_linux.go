24739fcd008cdbb1fbd4ac1b989a1a342e715bfb
Sascha Grunert <sgrunert@suse.com>
2019-06-25 10:47:30 +0200

    Change reload api to actually return a value
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- seccomp_linux.go
+++ seccomp_linux.go
@@ -37,10 +37,9 @@ func LoadProfileFromBytes(body []byte, rs *specs.Spec) (*specs.LinuxSeccomp, err
 	return setupSeccomp(config, rs)
 }
 
-// Reload takes a Seccomp struct and a spec to reload the config
-func Reload(config *Seccomp, specgen *specs.Spec) error {
-	_, err := setupSeccomp(config, specgen)
-	return err
+// LoadProfileFromConfig takes a Seccomp struct and a spec to retrieve a LinuxSeccomp
+func LoadProfileFromConfig(config *Seccomp, specgen *specs.Spec) (*specs.LinuxSeccomp, error) {
+	return setupSeccomp(config, specgen)
 }
 
 var nativeToSeccomp = map[string]Arch{
